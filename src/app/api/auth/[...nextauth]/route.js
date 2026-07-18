c508d8012170538a7be58f99a91979855ef11e24
prakash <prakashgajjar096@gmail.com>
2026-07-18 12:01:55 +0530

    fixed login

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -77,6 +77,23 @@ export const authOptions = {
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
 
+  jwt: {
+    maxAge: 30 * 24 * 60 * 60,
+  },
+
+  cookies: {
+    sessionToken: {
+      name: process.env.NODE_ENV === "production" ? "__Secure-next-auth.session-token" : "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 30 * 24 * 60 * 60, // 30 days persistent cookie
+      },
+    },
+  },
+
   secret: process.env.NEXTAUTH_SECRET,
 };
 
